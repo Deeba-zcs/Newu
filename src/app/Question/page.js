ab02e625890c1ace675b4c59cf97f2bab04f74e3
Deeba-zcs <deeba_zuberi@codenticsoftware.com>
2023-08-07 20:07:58 +0530

    selected .and array

--- src/app/Question/page.js
+++ src/app/Question/page.js
@@ -12,7 +12,11 @@ const Question = () => {
   const [errorMessage, setErrorMessage] = useState("");
   const progress = ((currentQuestion + 1) / totalQuestions) * 100;
   const [progressBarClass, setProgressBarClass] = useState();
+  const isTextQuestion = currentQuestionData.question_type === "text";
 
+  const [allSelectedOptionsData, setAllSelectedOptionsData] = useState([]);
+  const [showResult, setShowResult] = useState(false);
+  const [arrayoption, setarrayoption] = useState([]);
   useEffect(() => {
     setProgressBarClass(
       `progress-bar progress-bar-${Math.floor(progress / 25) * 25}`
@@ -24,27 +28,32 @@ const Question = () => {
     setSelectedOptions([]);
     setErrorMessage("");
   };
-
+  const handleTextInputChange = (event) => {
+    const { value } = event.target;
+    setSelectedOptions([value]);
+  };
   const handleOptionSelect = (optionIndex) => {
+    const selectedOption = currentQuestionData.questions_options[optionIndex];
+
     let updatedSelectedOptions;
+
     if (currentQuestionData.question_type === "radio") {
       updatedSelectedOptions = [optionIndex];
     } else {
       if (selectedOptions.includes(optionIndex)) {
         updatedSelectedOptions = selectedOptions.filter(
-          (index) => index !== optionIndex
+          (option) => option !== optionIndex
         );
       } else {
         updatedSelectedOptions = [...selectedOptions, optionIndex];
       }
     }
+
     setSelectedOptions(updatedSelectedOptions);
     setErrorMessage("");
   };
-
   const handleNextQuestion = (event) => {
-    event.preventDefault(); // Prevent form submission
-
+    event.preventDefault();
     if (selectedOptions.length === 0) {
       setErrorMessage(
         "Please select at least one option before proceeding to the next question."
@@ -53,7 +62,28 @@ const Question = () => {
       setErrorMessage("");
       const nextQuestion = currentQuestion + 1;
       if (nextQuestion < totalQuestions) {
+        // Push the selected options of the current question into the array
+        setAllSelectedOptionsData((prevData) => [
+          ...prevData,
+          selectedOptions.map((optionIndex) => {
+            return currentQuestionData.questions_options[optionIndex];
+          }),
+        ]);
         handleQuestionChange(nextQuestion);
+      } else {
+        // For the last question, also push the selected options into the array
+        setAllSelectedOptionsData((prevData) => [
+          ...prevData,
+          selectedOptions.map((optionIndex) => {
+            return currentQuestionData.questions_options[optionIndex];
+          }),
+        ]);
+
+        setShowResult(true);
+        console.log("All Selected Options Data:", allSelectedOptionsData);
+        allSelectedOptionsData.map((d) => {
+          return console.log("d.option", d.option);
+        });
       }
     }
   };
@@ -76,13 +106,7 @@ const Question = () => {
             ></div>
           </div>
           <div className="row">
-            <div
-              className="col-lg-8 "
-              // style={{
-              //   marginTop: "50px!important",
-              //   marginBottom: "50px!imporant",
-              // }}
-            >
+            <div className="col-lg-8">
               <p
                 className="font-poppins-regular"
                 style={{
@@ -103,65 +127,84 @@ const Question = () => {
                     </div>
                   </div>
                   <br />
-                  <div className="row" style={{ cursor: "pointer" }}>
-                    {currentQuestionData.questions_options.map(
-                      (option, index) => (
-                        <div
-                          className={`col-lg-5 opt col-md-6  ${
-                            selectedOptions.includes(index) ? "selected" : ""
-                          }`}
-                          key={index}
-                          onClick={() => handleOptionSelect(index)}
-                        >
-                          <div className="row" style={{ cursor: "pointer" }}>
-                            <div className="col-3 circle">
-                              {String.fromCharCode(65 + index)}
-                            </div>
-                            <div className="col-8 my-3">
-                              <div className="form-check option">
-                                <input
-                                  className="form-check-input chk"
-                                  type={
-                                    currentQuestionData.question_type ===
-                                    "radio"
-                                      ? "radio"
-                                      : "checkbox"
-                                  }
-                                  name={`question_${currentQuestion}`}
-                                  id={`option_${currentQuestion}_${index}`}
-                                  value={`option${index + 1}`}
-                                  checked={selectedOptions.includes(index)}
-                                  onChange={() => handleOptionSelect(index)}
-                                />
-                                <label
-                                  className="form-check-label rt"
-                                  htmlFor={`option_${currentQuestion}_${index}`}
-                                >
-                                  <strong>{option.option}</strong>
-                                </label>
+                  {isTextQuestion ? (
+                    <div className="row">
+                      <div className="col-lg-12">
+                        <input
+                          type="text"
+                          placeholder="Enter Text"
+                          className="txt"
+                          onChange={handleTextInputChange}
+                        />
+                      </div>
+                    </div>
+                  ) : (
+                    <div className="row" style={{ cursor: "pointer" }}>
+                      <>
+                        {currentQuestionData.questions_options.map(
+                          (option, index) => (
+                            <div
+                              className={`col-lg-5 opt col-md-6  ${
+                                selectedOptions.includes(index)
+                                  ? "selected"
+                                  : ""
+                              }`}
+                              key={index}
+                              onClick={() => handleOptionSelect(index)}
+                            >
+                              <div
+                                className="row"
+                                style={{ cursor: "pointer" }}
+                              >
+                                <div className="col-3 circle">
+                                  {String.fromCharCode(65 + index)}
+                                </div>
+                                <div className="col-8 my-3">
+                                  <div className="form-check option">
+                                    <input
+                                      className="form-check-input chk"
+                                      type={
+                                        currentQuestionData.question_type ===
+                                        "radio"
+                                          ? "radio"
+                                          : "checkbox"
+                                      }
+                                      name={`question_${currentQuestion}`}
+                                      id={`option_${currentQuestion}_${index}`}
+                                      value={`option${index + 1}`}
+                                      checked={selectedOptions.includes(index)}
+                                      onChange={() => handleOptionSelect(index)}
+                                    />
+                                    <label
+                                      className="form-check-label rt"
+                                      htmlFor={`option_${currentQuestion}_${index}`}
+                                    >
+                                      <strong>{option.option}</strong>
+                                    </label>
+                                  </div>
+                                </div>
+                                <div className="col-2">
+                                  {selectedOptions.includes(index) && (
+                                    <i
+                                      className="fa-solid fa-check fa-xl gb"
+                                      style={{ color: "#f1f4f8" }}
+                                    ></i>
+                                  )}
+                                </div>
                               </div>
                             </div>
-                            <div className="col-2">
-                              {selectedOptions.includes(index) && (
-                                <i
-                                  className="fa-solid fa-check fa-xl gb"
-                                  style={{ color: "#f1f4f8" }}
-                                ></i>
-                              )}
-                            </div>
-                          </div>
+                          )
+                        )}
+                        <div className="row my-3">
+                          <input
+                            type="text"
+                            placeholder="Enter Text"
+                            className="txt"
+                          />
                         </div>
-                      )
-                    )}
-                  </div>
-
-                  <div className="row my-3">
-                    <input
-                      type="text"
-                      placeholder="Enter Text"
-                      className="txt"
-                    />
-                  </div>
+                      </>
+                    </div>
+                  )}
                   <div className="row my-3">
                     {errorMessage && (
                       <p style={{ color: "red", marginBottom: "0" }}>
@@ -186,28 +229,15 @@ const Question = () => {
                       )}
                     </div>
                     <div className="col-lg-6 col-md-6 mb-3">
-                      {currentQuestion < totalQuestions - 1 ? (
-                        <button
-                          type="submit"
-                          className={`btnsv btnsv-next ${
-                            selectedOptions.length === 0 ? "disabled" : ""
-                          }`}
-                        >
-                          Next
-                          <i
-                            className="fa-solid fa-arrow-right ms-1"
-                            style={{ color: "#eeeff2" }}
-                          ></i>
-                        </button>
-                      ) : (
-                        <button type="submit" className="btnsv">
-                          Submit{" "}
-                          <i
-                            className="fa-solid fa-arrow-right"
-                            style={{ color: "#eeeff2" }}
-                          ></i>
-                        </button>
-                      )}
+                      <button type="submit" className="btnsv btnsv-next">
+                        {currentQuestion < totalQuestions - 1
+                          ? "Next"
+                          : "Submit"}{" "}
+                        <i
+                          className="fa-solid fa-arrow-right"
+                          style={{ color: "#eeeff2" }}
+                        ></i>
+                      </button>
                     </div>
                   </div>
                 </div>
